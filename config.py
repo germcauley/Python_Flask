970c7b43a51804014a42012ef23f82d2309a3053
Gerald McAuley <geraldmcauley@gwsteam.com>
2019-11-13 15:42:14 +0000

    email functionality working

--- config.py
+++ config.py
@@ -1,25 +1,22 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Set up mail server
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    # MAIL_SERVER = 'smtp@gmail.com'
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
     ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 25
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'geraldmcauleydev@gmail.com'
-    MAIL_PASSWORD = 'app password generated in step 3'
-
-    POSTS_PER_PAGE = 25
+    MAIL_USERNAME = 'add email'
+    MAIL_PASSWORD = 'add password'
