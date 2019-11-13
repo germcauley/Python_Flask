3e6b447e9ede698442048f15ae63809be6579e82
Gerald McAuley <geraldmcauley@gwsteam.com>
2019-11-13 16:07:20 +0000

    add bootstrap and stylings

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
@@ -15,6 +16,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
